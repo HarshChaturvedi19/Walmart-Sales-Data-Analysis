4a19e0021706ae00c25796de4562455444fd7f57
HarshChaturvedi19 <harshchaturvedi580@gmail.com>
2025-02-25 15:39:45 +0530

    Question 3-7

--- MySQL Queries.sql
+++ MySQL Queries.sql
@@ -39,3 +39,67 @@ WITH ranked AS (
 SELECT r.branch, r.category, r.avg_rating
 FROM ranked r
 WHERE r.rnk = 1;
+
+
+
+
+-- Q3: Identify the busiest day for each branch based on the number of transactions
+SELECT branch, formatted_date, day_of_week, transaction_count
+FROM (
+    SELECT 
+        branch,
+        STR_TO_DATE(date, '%d/%m/%Y') AS formatted_date,
+        DAYNAME(STR_TO_DATE(date, '%d/%m/%Y')) AS day_of_week,
+        COUNT(*) AS transaction_count,
+        RANK() OVER (PARTITION BY branch ORDER BY COUNT(*) DESC) AS rnk
+    FROM walmart
+    GROUP BY branch, formatted_date, day_of_week
+) AS ranked
+WHERE rnk = 1;
+
+
+-- Q4: Calculate the total quantity of items sold per payment method
+SELECT 
+    payment_method,
+    SUM(quantity) AS no_qty_sold
+FROM walmart
+GROUP BY payment_method;
+
+-- Q5: Determine the average, minimum, and maximum rating of categories for each city
+SELECT 
+    city,
+    category,
+    MIN(rating) AS min_rating,
+    MAX(rating) AS max_rating,
+    AVG(rating) AS avg_rating
+FROM walmart
+GROUP BY city, category;
+
+
+-- Q6: Calculate the total profit for each category
+SELECT 
+    category,
+    SUM(unit_price * quantity * profit_margin) AS total_profit
+FROM walmart
+GROUP BY category
+ORDER BY total_profit DESC;
+
+
+
+-- Q7: Determine the most common payment method for each branch
+WITH ranked_payments AS (
+    SELECT 
+        branch,
+        payment_method,
+        COUNT(*) AS total_trans,
+        RANK() OVER (PARTITION BY branch ORDER BY COUNT(*) DESC) AS rnk
+    FROM walmart
+    GROUP BY branch, payment_method
+)
+SELECT 
+    branch, 
+    payment_method AS preferred_payment_method, 
+    total_trans
+FROM ranked_payments
+WHERE rnk = 1
+ORDER BY branch;
