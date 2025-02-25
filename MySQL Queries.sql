0a61f2d5e6da2d2df2126aaa0e04d078df99da30
HarshChaturvedi19 <harshchaturvedi580@gmail.com>
2025-02-25 15:17:36 +0530

    Business Problem 1

--- MySQL Queries.sql
+++ MySQL Queries.sql
@@ -14,3 +14,11 @@ GROUP BY branch, city;
 -- maximum quantity
 SELECT category, MAX(quantity) AS max_quantity
 
+-- Business Problems 
+--  Q1: Find different payment methods, number of transactions, and quantity sold by payment method
+SELECT 
+    payment_method,
+    COUNT(*) AS no_payments,
+    SUM(quantity) AS no_qty_sold
+FROM walmart
+GROUP BY payment_method;
